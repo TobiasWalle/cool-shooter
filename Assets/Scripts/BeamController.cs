bfe12a6f1458db87cc2c718de70f078413a366d5
Tobias Walle <tobias.walle@maibornwolff.de>
2017-09-26 23:55:35 +0200

    Add laser to weapon

---
+++ Assets/Scripts/BeamController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class BeamController : MonoBehaviour {
+    public float duration = .5f;
+    public float maxWidth = .2f;
+
+    float width;
+    float timeActivated;
+    bool isActive = false;
+
+    LineRenderer lineRenderer;
+
+	void Update () {
+        if (!isActive) return;
+        float progressInPercent = GetProgress();
+        if (progressInPercent > 1) Destroy(gameObject); // Destroy the beam after one shot
+
+        width = maxWidth * (1 - progressInPercent);
+        lineRenderer.startWidth = width;
+        lineRenderer.endWidth = width;
+	}
+
+    public void Fire(Vector3 start, Vector3 end)
+    {
+        if (isActive) return;
+        lineRenderer = GetComponent<LineRenderer>();
+        Vector3[] positions = { start, end };
+        lineRenderer.SetPositions(positions);
+        SetActive(true);
+    }
+
+    void SetActive(bool active)
+    {
+        isActive = active;
+        lineRenderer.enabled = active;
+        if (active) timeActivated = Time.time;
+    }
+
+    float GetProgress()
+    {
+        return (Time.time - timeActivated) / duration;
+    }
+}
--- Assets/Scripts/InstagibController.cs
+++ Assets/Scripts/InstagibController.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InstagibController : MonoBehaviour {
+    public float cooldownTime = .5f;
+    public GameObject beamPrefab;
 
-    public float cooldownTime = 2f;
     float timeLastShot = 0;
 
-	void Update () {
+    void Update () {
         Vector3 forward = Vector3.forward * 100;
         Debug.DrawLine(transform.position, transform.position + forward, Color.red);
 	}
@@ -16,16 +17,33 @@ public class InstagibController : MonoBehaviour {
     {
         float timeSinceLastShot = Time.time - timeLastShot;
         if (timeSinceLastShot < cooldownTime) return;
-        print("Shot");
+
         timeLastShot = Time.time;
         RaycastHit hitInfo;
         if (Physics.Raycast(transform.position, Vector3.forward, out hitInfo))
         {
+            RenderBeam(hitInfo.point);
             GameObject gameObject = hitInfo.collider.gameObject;
             if (gameObject.tag == "Player")
             {
                 Destroy(gameObject);
             }
+        } else
+        {
+            RenderBeam(transform.position + Vector3.forward * 10000);
         }
+    }
+
+    void RenderBeam(Vector3 target)
+    {
+        GameObject instance = Instantiate(beamPrefab);
+        BeamController beamController = instance.GetComponent<BeamController>();
+        if (beamController == null)
+        {
+            Debug.LogError("The beam prefab requires a beam controller");
+            return;
+        }
+
+        beamController.Fire(transform.position, target);
     }
 }
