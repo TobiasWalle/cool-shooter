722dc970cc2d51d5bb4387cec44e3bb77f808ecd
Bartlomiej Pohl <bartlomiej.pohl@gmail.com>
2017-09-27 19:30:31 +0200

    Implemented first version of jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,25 @@ public class PlayerController : MonoBehaviour {
     InstagibController instagibController;
     CollisionFlags collisionFlags;
 
+    public float gravityAcceleration = -2.0f;
+    public float jumpAcceleration = 25.0f;
+    public float linearDamping = 0.7f;
+    public float superMarioScale = 8.0f;
+    public float maxJumpAccelerationTime = 0.1f;
+
+    private Vector3 currentVelocity = Vector3.zero;
+
+    private Vector3 BottomPosition
+    {
+        get
+        {
+            Vector3 bottomPos = this.transform.position;
+            bottomPos.y -= this.characterController.height * 0.5f;
+
+            return bottomPos;
+        }
+    }
+
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
@@ -22,13 +41,38 @@ public class PlayerController : MonoBehaviour {
 
     public void Move(Vector3 direction)
     {
-        Vector3 motion = direction * speed * Time.deltaTime;
-        collisionFlags = characterController.Move(motion);
+        collisionFlags = characterController.Move(direction);
+    }
+
+    private void integrateMovement()
+    {
+        this.Move(currentVelocity);
+
+        float gravityScale = currentVelocity.y < 0.0f ? superMarioScale : 1.0f;
+
+        currentVelocity.y += gravityAcceleration * gravityScale * Time.deltaTime;
+        currentVelocity *= linearDamping;
+    }
+
+    public bool isGrounded()
+    {
+        const float rayMaxDistance = 0.2f;
+
+        Ray r = new Ray();
+        r.origin = this.BottomPosition;
+        r.direction = Vector3.down;
+
+        return Physics.Raycast(r, rayMaxDistance);
+    }
+
+    public void Update()
+    {
+        this.integrateMovement();
     }
 
     public void Jump()
     {
-        // TODO
+        currentVelocity.y += jumpAcceleration * Time.deltaTime;
     }
 
     public void Shot()
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -8,6 +8,9 @@ public class PlayerInput : NetworkBehaviour {
     public string horizontalAxis = "Horizontal";
     public string verticalAxis = "Vertical";
 
+    private bool jumpButtonPressedLastFrame = false;
+    private float timePassedSinceFirstButtonPress = 0.0f;
+
     PlayerController controller;
 
     private void Start()
@@ -18,12 +21,30 @@ public class PlayerInput : NetworkBehaviour {
     void Update () {
         if (!isLocalPlayer) return;
         Vector3 direction = new Vector3(Input.GetAxisRaw(horizontalAxis), 0, Input.GetAxisRaw(verticalAxis));
-        controller.Move(direction);
+        controller.Move(direction * controller.speed * Time.deltaTime);
 
         if (Input.GetAxisRaw("Fire1") == 1)
         {
             controller.Shot();
         }
+
+        bool jumpDown = Input.GetAxisRaw("Jump") == 1;
+        bool grounded = controller.isGrounded();
+
+        bool isFirstPress = jumpDown && !jumpButtonPressedLastFrame;
+
+        if (isFirstPress && grounded)
+        {
+            timePassedSinceFirstButtonPress = 0.0f;
+        }
+
+        if (jumpDown && timePassedSinceFirstButtonPress < this.controller.maxJumpAccelerationTime)
+        {
+            this.controller.Jump();
+        }
+
+        timePassedSinceFirstButtonPress += Time.deltaTime;
+        jumpButtonPressedLastFrame = jumpDown;
 	}
 
     public override void OnStartLocalPlayer()
