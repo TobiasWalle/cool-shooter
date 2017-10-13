3b4e0233f774d32384a624b47d265f61bb8ec8dc
Tobias Walle <tobias.walle@maibornwolff.de>
2017-10-14 00:22:17 +0200

    Add cooldown time after dead

--- Assets/Scripts/HealthControl.cs
+++ Assets/Scripts/HealthControl.cs
@@ -6,74 +6,95 @@ using UnityEngine.Networking;
 
 public class HealthControl : NetworkBehaviour {
 
-	private NetworkStartPosition[] spawnPoints;
 
-	[SyncVar]
-	private int _deathCounter = 0;
 	[SyncVar]
 	public int killCount = 0;
+	public int maxHealth;
+    public int coolDownTime;
 
 	[SyncVar]
-	private int _currentHealth;
-	public int maxHealth;
+	private int deathCounter = 0;
+	[SyncVar]
+	private int currentHealth;
+	private NetworkStartPosition[] spawnPoints;
+    private bool isDead = false;
+    private float coolDownTimeLeft = 0f;
 
 	void Start()
 	{
-		_currentHealth = maxHealth;
+		currentHealth = maxHealth;
 		if(isLocalPlayer)
 		{
 			spawnPoints = FindObjectsOfType<NetworkStartPosition>();
 		}
 	}
 
-	public void TakeDamage(int amount, GameObject damager)
+    private void Update()
+    {
+        if (isDead)
+        {
+            if (coolDownTimeLeft > 0)
+            {
+                coolDownTimeLeft -= Time.deltaTime;
+            } else
+            {
+                RpcRespawn();
+            }
+        }
+    }
+
+    public void TakeDamage(int amount, GameObject damager)
 	{
 		if(!isServer) 
 		{
 			return;
 		}
 
-		Debug.Log(damager.name);
-
-		_currentHealth -= amount;
-		if (_currentHealth <= 0) 
+		currentHealth -= amount;
+		if (currentHealth <= 0) 
 		{
-			_currentHealth = maxHealth;
-			_deathCounter++;
 			var otherHealth = damager.GetComponent<HealthControl>();
-			if (otherHealth == null)
-			{
-				Debug.Log("Damager Health Control Script not found");
-			} 
-			else
-			{
-				otherHealth.killCount++;
-			}
-			RpcRespawn();
+            Die(otherHealth);
 		}
 	}
+
+    void Die(HealthControl killerHealthControl)
+    {
+        if (killerHealthControl == null)
+        {
+            Debug.LogError("Killer Health Control Script not found");
+            return;
+        }
+        deathCounter++;
+        killerHealthControl.killCount++;
+
+        coolDownTimeLeft = coolDownTime;
+        isDead = true;
+    }
 		
 	[ClientRpc]
 	void RpcRespawn()
 	{
 		if(isLocalPlayer)
 		{
+			currentHealth = maxHealth;
 			Vector3 spawnPoint = Vector3.zero;
 			if(spawnPoints != null && spawnPoints.Length > 0)
 			{
 				spawnPoint = spawnPoints [Random.Range (0, spawnPoints.Length)].transform.position;
 			}
 			transform.position = spawnPoint;
+            isDead = false;
 		}
 	}
 
 	public int GetCurrentHealth()
 	{
-		return _currentHealth;
+		return currentHealth;
 	}
 
 	public int GetDeathCount()
 	{
-		return _deathCounter;
+		return deathCounter;
 	}
 }
